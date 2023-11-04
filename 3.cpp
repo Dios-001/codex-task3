111449ef7c98c7d586c0de0c930d6672a918eb73
Dios-001 <abhishekpatawari.codes@gmail.com>
2023-11-04 11:07:30 +0530

    2nd commit

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+inline int addNumbers(int a, int b){
+    return a+b;
+}
+int multiplyNumbers(int x, int y, int z=1){
+    return x*y*z;
+}
+int main(){
+    int sum = addNumbers(5,7);
+    int product = multiplyNumbers(3,4);
+    std::cout << "Addition of numbers: " << sum << '\n';
+    std::cout << "Multiplication of number " << product;
+
+}
